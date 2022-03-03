d67517e7382cdc82e1f1f1b4f47faf6b71dacafc
dogusakkas <dogusakkas@gmail.com>
2022-03-03 16:20:24 +0300

    DataAccessLayer katmanında abstract, entityframework ve repository alanların oluşturulması

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
